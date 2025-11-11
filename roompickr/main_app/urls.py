3968426f930c5f164cb509427420f92a67c9bc59
jamsaeed <jameela.mohamed.saeed@gmail.com>
2025-11-11 12:30:35 +0300

    registration html

--- roompickr/main_app/urls.py
+++ roompickr/main_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('')
+    path(''),
 ]
 
