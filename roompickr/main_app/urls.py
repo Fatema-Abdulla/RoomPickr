f8f0882d1a7ddff9237959b8019ac4a70d4cb660
jamsaeed <jameela.mohamed.saeed@gmail.com>
2025-11-11 12:43:43 +0300

    fix base login and logout

--- roompickr/main_app/urls.py
+++ roompickr/main_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path(''),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
 
