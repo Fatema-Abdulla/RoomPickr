b9050b2f0976233d9be1c82b5576623939421f09
Fatema-Abdulla <Darksweet202@gmail.com>
2025-11-12 09:41:08 +0300

    finish urls and html files

--- roompickr/main_app/urls.py
+++ roompickr/main_app/urls.py
@@ -8,5 +8,11 @@ urlpatterns = [
     path("rooms/", views.rooms_index, name="index"),
     path('accounts/profile/', views.profile, name='profile'),
     path('accounts/profile/<int:profile_id>/update/', views.update_profile, name='update_profile'),
+
+    path("spaces/", views.SpaceList.as_view(), name="spaces_index"),
+    path("spaces/<int:pk>/", views.SpaceDetail.as_view(), name="spaces_detail"),
+    path("spaces/create/", views.SpaceCreate.as_view(), name="spaces_create"),
+    path("spaces/<int:pk>/update/", views.SpaceUpdate.as_view(), name="spaces_update"),
+    path("spaces/<int:pk>/delete/", views.SpaceDelete.as_view(), name="spaces_delete"),
 ]
 
